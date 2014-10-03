64f39db9aa06af0a3d5cfc2deb9b62c376fc7919
Sergey Nizovtsev <snizovtsev@gmail.com>
2014-10-03 16:35:03 +0400

    Fix endianness detection on OS X
    Signed-off-by: Sergey Nizovtsev <snizovtsev@gmail.com>

--- fluid/util/util.h
+++ fluid/util/util.h
@@ -94,6 +94,17 @@
 #define CONTAINER_OF(POINTER, STRUCT, MEMBER)                           \
         ((STRUCT *) ((char *) (POINTER) - offsetof (STRUCT, MEMBER)))
 
+/* Check endianness on OS X. */
+#ifndef __BYTE_ORDER
+#define __BYTE_ORDER __BYTE_ORDER__
+#endif
+#ifndef __BIG_ENDIAN
+#define __BIG_ENDIAN __ORDER_BIG_ENDIAN__
+#endif
+#ifndef __LITTLE_ENDIAN
+#define __LITTLE_ENDIAN __ORDER_LITTLE_ENDIAN__
+#endif
+
 #ifdef  __cplusplus
 extern "C" {
 #endif
